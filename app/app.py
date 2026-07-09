716d32de6636baba5ae3ed573f3ac961d61f6bdd
Anurag Tamrakar <anuragtamrakar415@gmail.com>
2026-07-09 17:04:21 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,10 +11,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+         "https://smart-todo-api-gvx2.onrender.com",
     ],
+     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
